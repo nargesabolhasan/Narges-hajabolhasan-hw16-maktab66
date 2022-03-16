45ab0327bd1637dd1236caf4bf19750b0ec4c387
narges Hajabolhasan <narges.abolhasan@gmail.com>
2022-03-16 20:35:55 +0330

    input 2 ref added

--- 1/my-app/src/component/useRefExample.js
+++ 1/my-app/src/component/useRefExample.js
@@ -1,34 +1,40 @@
-import { useEffect ,useState,useCallback,memo} from "react";
+import { useEffect, useState, useCallback, memo, useRef } from "react";
 
 function UseRefExample() {
   console.log("App is rendering...");
   useEffect(() => console.log("== App rendered =="));
   const [firstInput, setFirstInput] = useState();
   const [secondInput, setSecondInput] = useState();
-  const input1 = (null);
-  const input2 = (null);
+  let input1 = useRef();
+  let input2 = useRef();
+  const changeValue1 = useCallback(() => {
+    setFirstInput(input1.current.value)
+  }, [firstInput]);
 
-  const changeValue1 = useCallback(() => {setFirstInput(input1.current.outerText)},[firstInput]);
+  const changeValue2 = useCallback(() => {
+    setSecondInput(input2.current.value)
+  }, [secondInput]);
 
-  const changeValue2 = useCallback(() => {setSecondInput(input2.current.outerText)}, [secondInput]);
 
   return (
     <div className="App">
       <div className="value-1"  >
-        first value: <h1>{firstInput}</h1>
+        first value: <h5>{firstInput}</h5>
         <br />
         change first value:
-        <input ref={input1} />
-        <button onClick={changeValue1}> change </button>
+        <input ref={input1} type="text" />
+        <button type="button" onClick={changeValue1}> change </button>
       </div>
-      <div className="value-2" >
-        second value: <h1>{secondInput}</h1>
+
+      <div className="value-2"  >
+        Second value: <h5>{secondInput}</h5>
         <br />
-        change second value:
-        <input ref={input2} />
-        <button onClick={changeValue2}> change </button>
+        change Second value:
+        <input ref={input2} type="text" />
+        <button type="button" onClick={changeValue2}> change </button>
       </div>
     </div>
   );
 }
 export default memo(UseRefExample);
+
